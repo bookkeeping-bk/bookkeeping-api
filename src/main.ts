e3289d4c4790d53d0a05c322327880f43ceb9748
YouJie <youjiefun@gmail.com>
2020-04-02 14:00:33 +0800

    feat:新增 .env 配置 feat:新增swagger feat:新增脚本命令 fix:更新包版本

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
+import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { Logger } from '@nestjs/common';
 import { AppModule } from './app.module';
+
+const port = process.env.PORT || 3000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const options = new DocumentBuilder()
+    .setTitle('Cats example')
+    .setDescription('The cats API description')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(port);
+  Logger.log(`Server running on http://localhost:${port}`, 'Bootstrap');
 }
 bootstrap();
