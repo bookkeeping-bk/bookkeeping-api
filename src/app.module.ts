5909288ac6f689e1e36046eeb778a85a10c6fe63
YouJie <youjiefun@gmail.com>
2020-04-10 09:42:42 +0800

    feat:新增用户表 fix:优化orm 路径

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import * as ormconfig from './database/ormconfig.js';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(ormconfig)],
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
+  imports: [TypeOrmModule.forRoot(require('./database/ormconfig'))],
   controllers: [],
   providers: [],
 })
---
+++ src/database/migrations/1586419973378-create-users.ts
@@ -0,0 +1,69 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class CreateUsers1586419973378 implements MigrationInterface {
+  private usersTable = new Table({
+    name: 'users',
+    columns: [
+      {
+        name: 'id',
+        type: 'integer',
+        isPrimary: true,
+        isGenerated: true,
+        generationStrategy: 'increment',
+      },
+      {
+        name: 'username',
+        type: 'varchar',
+        length: '32',
+        isNullable: false,
+        comment: '用户名',
+      },
+      {
+        name: 'password',
+        type: 'varchar',
+        length: '64',
+        isNullable: false,
+        comment: '密码',
+      },
+      {
+        name: 'mobile',
+        type: 'char',
+        length: '11',
+        isUnique: true,
+        isNullable: false,
+        comment: '手机号',
+      },
+      { name: 'login_ip', type: 'varchar', length: '255', comment: '登录IP' },
+      { name: 'reg_ip', type: 'varchar', length: '255', comment: '注册IP' },
+      {
+        name: 'latest_online_at',
+        type: 'timestamptz',
+        isNullable: false,
+        default: 'now()',
+        comment: '最后一次在线时间',
+      },
+      {
+        name: 'created_at',
+        type: 'timestamptz',
+        isNullable: false,
+        default: 'now()',
+        comment: '创建时间',
+      },
+      {
+        name: 'updated_at',
+        type: 'timestamptz',
+        isNullable: false,
+        default: 'now()',
+        comment: '更新时间',
+      },
+    ],
+  });
+
+  public async up(queryRunner: QueryRunner): Promise<any> {
+    await queryRunner.createTable(this.usersTable);
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<any> {
+    await queryRunner.dropTable(this.usersTable);
+  }
+}
