a056561efd7e5d81ab1b3affb4397a99eed7b2b9
YouJie <youjiefun@gmail.com>
2020-04-16 20:46:21 +0800

    fix:修改数据库配置文件路径

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import * as ormconfig from './database/ormconfig';
+import * as ormconfig from './config/ormconfig';
 
 import { AuthModule } from './modules/auth/auth.module';
 
